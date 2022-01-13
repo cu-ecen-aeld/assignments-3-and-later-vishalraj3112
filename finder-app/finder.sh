b5adde7e9620853153b69397131037eab04d3c35
vishalraj3112 <vishal.raj15@siesgst.ac.in>
2022-01-12 22:00:02 -0700

    finder written and tests conducted

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 filesdir=$1 #1 argument is the file directory path
 searchstr=$2 #2 argument is the string to be searched
-if [ "$#" -eq 0 ]; then 
-	echo  "No arguments"
+
+#argument number checking
+if [ "$#" -eq 0 ] || [ "$#" -eq 1 ]; then 
+	echo  "Error: Wrong arguments!"
 	exit 1
 fi
-if ! [ -d "$1" ]; then 
-	echo "$1 not a directory"
+
+if ! [ -d "$filesdir" ]; then #checking valid directory
+	echo "Error : $filesdir is not a directory on the FileSystem!"
 	exit 1 
 fi
 
-echo "string search:"
-grep -rnwc $1 -e $2
+X="$(find "$filesdir" -type f | wc -l)" #https://devconnected.com/how-to-count-files-in-directory-on-linux
 
-echo "file count:" 
-ls $1 | wc -l
+Y="$(grep -rnw "$filesdir" -e "$searchstr" | wc -l)" #https://stackoverflow.com/questions/16956810/how-do-i-find-all-files-containing-specific-text-on-linux
+#grep -rnw "$filesdir" -e "$searchstr" #uncomment this to see string match results
+
+#print final result
+echo "The number of files are $X and the number of matching lines are $Y"
 
